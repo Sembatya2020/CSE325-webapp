6d94dd1c22840bac68f95ffaa69a941ad5e75be1
Sembatya2020 <ivansmbt@gmail.com>
2025-11-05 19:23:35 +0300

    Update static web assets and enhance form input styling
    - Updated static web asset cache and development JSON files to reflect recent changes.
    - Added padding to form inputs (input, textarea, select) in site.css for improved styling as per rubric requirements.

--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3112e800c7dadf6ef263d19c0e47703ca046a739")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
